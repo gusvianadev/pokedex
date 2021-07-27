dbceb331feb3f9215e17c00f53db1729e737e594
gusvianadev <gusvianadev@gmail.com>
2021-07-26 21:13:29 -0300

    feature: search pokemon variants
    I had to first check if the user searched for a name or id, then if it was a name, check if it has spaces on it and replace them with '-', then check if it says 'alolan' or 'galarian' and replace them with 'alola' and 'galar', then fetch the pokemon that way, if it fails, it means two things: the user wrote a wrong nam/id, or they are looking for a variant, so I check the latter, reverting the order (because pokemon-species doesn't have variants). If it still fails, it means that they wrote the name backwards (e.g. 'alola raichu' instead of 'raichu alola', the variant must always go second), so I reverse the name and search again. If it still fails, it means the user did in fact write a wrong name/id

--- src/redux/thunkSinglePoke.js
+++ src/redux/thunkSinglePoke.js
@@ -1,31 +1,91 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { setupCache } from 'axios-cache-adapter';
 
 const getSinglePoke = createAsyncThunk(
 	'singlePoke/getSinglePoke',
 	async (pokeToSearch) => {
-		// cache setter
-		const cache = setupCache({
-			maxAge: 604800000,
-		});
-		const getData = axios.create({
-			adapter: cache.adapter,
-		});
+		// VARIABLES //
+		const pokeDataUrl = 'https://pokeapi.co/api/v2/pokemon/';
+		const speciesUrl = 'https://pokeapi.co/api/v2/pokemon-species/';
+		let pokeData;
+		let speciesData;
+		const fixedNameStr = () => {
+			let toFix = pokeToSearch;
 
-		const pokeData = (
-			await getData({
-				url: `https://pokeapi.co/api/v2/pokemon/${pokeToSearch}`,
-				method: 'get',
-			})
-		).data;
+			// first check if the user is searching for a name or id
+			if (typeof pokeToSearch !== 'number') {
+				// this replaces spaces with '-'
+				const removeSpaces = toFix.replace(' ', '-');
+				toFix = removeSpaces;
 
-		const speciesData = (
-			await getData({
-				url: `https://pokeapi.co/api/v2/pokemon-species/${pokeToSearch}`,
-				method: 'get',
-			})
-		).data;
+				const fixedName = toFix
+					.split('-') // transform the str into arr
+					.map((element) => {
+						// check if the user wrote 'galarian' or 'alolan'
+						if (element === 'galarian') {
+							return 'galar';
+						}
+
+						if (element === 'alolan') {
+							return 'alola';
+						}
+
+						return element;
+					})
+					.join('-'); // transform the array back to string
+
+				return fixedName;
+			}
+
+			return pokeToSearch;
+		};
+
+		// FUNCTIONS //
+		const fetchSpeciesFirst = async (poke) => {
+			const resSpeciesData = await axios(`${speciesUrl}${poke}`);
+			const resPokeData = await axios(
+				`${pokeDataUrl}${resSpeciesData.data.id}`
+			);
+
+			speciesData = await resSpeciesData.data;
+			pokeData = await resPokeData.data;
+		};
+
+		const fetchPokeDataFirst = async (poke) => {
+			const resPokeData = await axios(`${pokeDataUrl}${poke}`);
+			const resSpeciesData = await axios(
+				`${speciesUrl}${resPokeData.data.species.name}`
+			);
+
+			pokeData = await resPokeData.data;
+			speciesData = await resSpeciesData.data;
+		};
+		try {
+			// first just fetch the fixed name
+			await fetchSpeciesFirst(fixedNameStr());
+		} catch (e) {
+			// if it fails, it means that the user wrote a wrong name/id or
+			// that they're searching for a pokemon variant, so let's search a variant
+			try {
+				// it's the same, but you have to search the pokemon first
+				// because variants can't be searched in species
+				await fetchPokeDataFirst(fixedNameStr());
+			} catch (e2) {
+				// if it didn't work, it means that the user wrote a wrong name/id
+				// or they're searching in the wrong order (e.g 'alola raichu' instead of 'raichu alola')
+				// so, let's reverse that order
+				const reversedName = fixedNameStr().split('-').reverse();
+
+				try {
+					// fetch the reversed name
+					await fetchPokeDataFirst(reversedName.join('-'));
+				} catch (e3) {
+					// if it didn't work, it means that the user wrote a
+					// wrong name/id
+					throw new Error(e3);
+				}
+			}
+		}
 
 		return { pokeData, speciesData };
 	}
